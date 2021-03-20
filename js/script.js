b1788199657886baadae3239a811f6b1d5dc1c2f
Potato Gandalf <potatogandalf1151@gmail.com>
2021-03-20 13:03:28 +0600

    Add smooth background darkening

--- js/script.js
+++ js/script.js
@@ -3,5 +3,9 @@
 let openSidebarButton = document.querySelector(".nav__bar__sidebar_button");
 let sidebar = document.querySelector(".sidebar__wrapper");
 openSidebarButton.addEventListener("click", function() {
+    let pageMask = document.createElement("div");
+    pageMask.classList.add("page__mask");
+    setTimeout(() => pageMask.classList.add("page__mask__active"), 100);
+    document.body.append(pageMask);
     sidebar.classList.add("sidebar-visible");
 });
