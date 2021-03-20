950cf8f1c304657fde2f7529c35ce113a2314931
Potato Gandalf <potatogandalf1151@gmail.com>
2021-03-20 16:52:51 +0600

    Reduce hatch animation time and rotation

--- js/script.js
+++ js/script.js
@@ -8,13 +8,13 @@ openSidebarBtn.addEventListener("click", function () {
   // TODO: Add support for variable animation duration by getComputedStyle checking.
   navbarHatch.classList.add("navbar__hatch-open");
   setTimeout(() => {
-  let pageMask = document.createElement("div");
-  document.body.append(pageMask);
+    let pageMask = document.createElement("div");
+    document.body.append(pageMask);
 
-  pageMask.classList.add("page__mask");
-  // Set it with a delay because transition doesn't seem to work otherwise.
-  setTimeout(() => pageMask.classList.add("page__mask__active"), 10);
+    pageMask.classList.add("page__mask");
+    // Set it with a delay because transition doesn't seem to work otherwise.
+    setTimeout(() => pageMask.classList.add("page__mask__active"), 10);
 
-  sidebar.classList.add("sidebar-visible");
-  }, 1000);
+    sidebar.classList.add("sidebar-visible");
+  }, 700);
 });
