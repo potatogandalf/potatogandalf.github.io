9e5c3df24fc05650006e5e8fd8b7372dacfe7e85
Potato Gandalf <potatogandalf1151@gmail.com>
2021-03-20 15:53:31 +0600

    Change sidebar open mask DOM insertion order

--- js/script.js
+++ js/script.js
@@ -4,8 +4,9 @@ let openSidebarBtn = document.querySelector(".navbar__sidebar_btn");
 let sidebar = document.querySelector(".sidebar");
 openSidebarBtn.addEventListener("click", function() {
     let pageMask = document.createElement("div");
-    pageMask.classList.add("page__mask");
-    setTimeout(() => pageMask.classList.add("page__mask__active"), 100);
     document.body.append(pageMask);
+    pageMask.classList.add("page__mask");
+    // Set it with a delay because transition doesn't seem to work otherwise.
+    setTimeout(() => pageMask.classList.add("page__mask__active"), 10);
     sidebar.classList.add("sidebar-visible");
 });
