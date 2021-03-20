906a42bcfda35ffc6fb8dd3b22d2eb34d2bf8551
Potato Gandalf <potatogandalf1151@gmail.com>
2021-03-20 16:45:56 +0600

    Add hatch trigger on hatch click

--- js/script.js
+++ js/script.js
@@ -2,11 +2,19 @@
 
 let openSidebarBtn = document.querySelector(".navbar__sidebar_btn");
 let sidebar = document.querySelector(".sidebar");
-openSidebarBtn.addEventListener("click", function() {
-    let pageMask = document.createElement("div");
-    document.body.append(pageMask);
-    pageMask.classList.add("page__mask");
-    // Set it with a delay because transition doesn't seem to work otherwise.
-    setTimeout(() => pageMask.classList.add("page__mask__active"), 10);
-    sidebar.classList.add("sidebar-visible");
+let navbarHatch = document.querySelector(".navbar__hatch");
+
+openSidebarBtn.addEventListener("click", function () {
+  // TODO: Add support for variable animation duration by getComputedStyle checking.
+  navbarHatch.classList.add("navbar__hatch-open");
+  setTimeout(() => {
+  let pageMask = document.createElement("div");
+  document.body.append(pageMask);
+
+  pageMask.classList.add("page__mask");
+  // Set it with a delay because transition doesn't seem to work otherwise.
+  setTimeout(() => pageMask.classList.add("page__mask__active"), 10);
+
+  sidebar.classList.add("sidebar-visible");
+  }, 1000);
 });
