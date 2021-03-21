39a2465a21230169a1a275f427217556274ccee5
Potato Gandalf <potatogandalf1151@gmail.com>
2021-03-21 14:44:16 +0600

    Fix delayed page mask reset

--- js/script.js
+++ js/script.js
@@ -2,7 +2,6 @@
 
 let openSidebarBtn = document.querySelector(".navbar__sidebar_btn");
 let sidebar = document.querySelector(".sidebar");
-
 openSidebarBtn.addEventListener("click", function () {
   let timeline = gsap.timeline();
   timeline
@@ -16,17 +15,21 @@ openSidebarBtn.addEventListener("click", function () {
     );
 });
 
-sidebar.addEventListener("keydown", function (e) {
-  if (e.keyCode === 27) {
+document.addEventListener("keydown", function (e) {
+  if (e.keyCode == 27) {
     let timeline = gsap.timeline();
     timeline
-      .to(".sidebar", { duration: 0.7, yPercent: -120, visibility: "hidden" })
-      .to(
-        ".page__mask",
-        { duration: 0.5, opacity: 0, visibility: "hidden" },
-        "<"
-      )
+      .to(".sidebar", { duration: 0.7, yPercent: -120 })
+      .to(".page__mask", { duration: 0.5, opacity: 0 }, "<")
+      // Use a separate visibilty animator because
+      // Animating both opacity and visibility
+      // at the same time makes the element disappear immediately.
+      // Why not set the elem.style.visibility later on?
+      // Well, the page would be inaccessible for 0.7 seconds
+      // while the hatch swung.
+      .set(".page__mask", { visibility: "hidden" })
       .to(".navbar__hatch", { duration: 0.7, rotation: 0 });
+
     e.preventDefault();
   }
 });
