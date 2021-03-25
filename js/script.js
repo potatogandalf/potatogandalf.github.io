048f710372d983513011e696e7c4ab8e05631a75
Potato Gandalf <potatogandalf1151@gmail.com>
2021-03-25 16:21:47 +0600

    this commit is this project's magnum opus. Reduced eww-factor by 100 lines.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,39 @@
 "use strict";
 
+const sidebarAnimation = gsap
+  .timeline({ paused: true })
+  .to(".navbar__hatch", { rotation: 90 })
+  .fromTo(
+    ".sidebar",
+    { xPercent: 0, yPercent: -120 },
+    { visibility: "visible", yPercent: 0 }
+  );
+
+const sidebarAnimationMobile = gsap
+  .timeline({ paused: true })
+  .fromTo(
+    ".sidebar",
+    { xPercent: -100, yPercent: 0 },
+    { visibility: "visible", xPercent: 0 },
+    "<"
+  );
+
+const modalAnimationMobile = gsap
+  .timeline({ paused: true })
+  .fromTo(
+    ".theme_picker__modal",
+    { yPercent: -120, visibility: "visible" },
+    { yPercent: 0 }
+  );
+
+const modalAnimation = gsap
+  .timeline({ paused: true })
+  .fromTo(
+    ".modal",
+    { yPercent: -100, opacity: 0, visibility: "visible" },
+    { duration: 0.5, yPercent: -50, opacity: 1 }
+  );
+
 let responsiveDetector = document.getElementById("js-responsive-detector");
 
 // You will see this function used too many times in this code.
@@ -71,104 +105,39 @@ document.addEventListener("click", function (e) {
   }
 });
 
-/* Need two flags because:
- if isSidebarOpen is used for opening *and* closing
-   if isSidebarOpen is set before animation completes
-     if user clicks fast enough twice
-       hatch opens and closes immediately
+function openOverlay(...overlayAnimations) {
+  let timeline = gsap.timeline({ defaults: { duration: 0.5 } });
+  overlayAnimations.forEach((animation) => {
+    timeline.add(() => animation.play());
+  });
+  timeline.to(
+    ".page__mask",
+    {
+      opacity: 0.5,
+      visibility: "visible",
+    },
+    "<"
+  );
+  return timeline;
+}
 
-    or if isSidebarOpen is set after the animation completes 
-      if user clicks fast enough twice 
-        hatch opens twice at the same time and glitches.
-*/
-let isSidebarOpening = false;
-let isSidebarOpen = false;
+function closeOverlay(timeline) {
+  timeline.reverse();
+}
 
 // The entire hatch is a button because clicking on corners doesn't work
 // with the padding when the button is nested within the hatch.
 let openSidebarBtn = document.querySelector(".navbar__hatch");
 
-// Why is sidebar not an Overlay?
-// Because it's different. It's not kosher to force it into an Overlay.
-
 openSidebarBtn.addEventListener("click", function () {
-  if (!isSidebarOpening) {
-    isSidebarOpening = true;
-
-    let timeline = gsap.timeline({
-      onComplete: () => {
-        isSidebarOpening = false;
-        isSidebarOpen = true;
-      },
-    });
-
-    if (checkOnMobile()) {
-      timeline
-        .to(".page__mask", {
-          duration: 0.4,
-          opacity: 0.4,
-          visibility: "visible",
-        })
-        .fromTo(
-          ".sidebar",
-          { xPercent: -100, yPercent: 0 }, // Reset yPercent after desktop animation.
-          { duration: 0.4, visibility: "visible", xPercent: 0 },
-          "<"
-        );
-    } else {
-      timeline
-        .to(".navbar__hatch", { duration: 0.7, rotation: 90 })
-        .to(".page__mask", {
-          duration: 0.5,
-          opacity: 0.5,
-          visibility: "visible",
-        })
-        .fromTo(
-          ".sidebar",
-          { xPercent: 0, yPercent: -120 }, // Reset xPercent after mobile animation.
-          { duration: 0.7, visibility: "visible", yPercent: 0 },
-          "<"
-        );
-    }
-  }
+  let animation = checkOnMobile() ? sidebarAnimationMobile : sidebarAnimation;
+  console.log(animation);
+  openOverlay(animation);
 });
 
-let isSidebarClosing = false;
-
-function closeSidebar() {
-  if (isSidebarOpen && !isSidebarClosing) {
-    isSidebarClosing = true;
-
-    let timeline = gsap.timeline({
-      onComplete: () => {
-        isSidebarOpen = false;
-        isSidebarClosing = false;
-      },
-    });
-
-    if (checkOnMobile()) {
-      timeline
-        .set(".navbar__hatch", { rotation: 0 }) // If left open by desktop mode.
-        .to(".sidebar", { duration: 0.4, xPercent: -100 })
-        .to(".page__mask", { duration: 0.4, opacity: 0 }, "<")
-        .set(".page__mask", { visibility: "hidden" });
-    } else {
-      timeline
-        .to(".sidebar", { duration: 0.7, yPercent: -120 })
-        .to(".page__mask", { duration: 0.5, opacity: 0 }, "<")
-        .set(".page__mask", { visibility: "hidden" })
-        // If not opened on mobile
-        .fromTo(
-          ".navbar__hatch",
-          { rotation: 90 },
-          { duration: 0.7, rotation: 0 }
-        );
-    }
-  }
-}
-
+/*
 let sidebarCloseBtn = document.querySelector(".sidebar__close_btn");
-sidebarCloseBtn.addEventListener("click", closeSidebar);
+sidebarCloseBtn.addEventListener("click", closeSidebar);*/
 
 let themeSwitcherBtn = document.querySelector(".theme_picker_btn");
 
@@ -190,9 +159,6 @@ function resetTheme() {
     .setAttribute("disabled", "disabled");
 }
 
-// Global because there can only be one overlay at any given time.
-let overlay;
-
 function createThemePreview(theme) {
   let preview = document.createElement("div");
   preview.classList.add("theme_picker__preview");
@@ -202,7 +168,6 @@ function createThemePreview(theme) {
   return preview;
 }
 
-let isOverlayOpen = false;
 let themes = [
   {
     themeName: "Lights Out",
@@ -217,77 +182,10 @@ let themes = [
 ];
 
 themeSwitcherBtn.addEventListener("click", function () {
-  if (!isOverlayOpen) {
-    let themePicker = document.createElement("div");
-    themePicker.classList.add("overlay", "modal", "theme_picker");
-    themes.forEach((theme) => {
-      themePicker.appendChild(createThemePreview(theme));
-    });
-    document.body.appendChild(themePicker);
-    if (checkOnMobile()) {
-      gsap
-        .timeline()
-        .fromTo(
-          ".modal",
-          { yPercent: -120, visibility: "visible" },
-          { duration: 0.3, yPercent: 0 }
-        )
-        .to(
-          ".page__mask",
-          {
-            duration: 0.5,
-            opacity: 0.5,
-            visibility: "visible",
-          },
-          "<"
-        );
-    } else {
-      console.log("not mox");
-      gsap
-        .timeline()
-        .fromTo(
-          ".modal",
-          { yPercent: -100, opacity: 0, visibility: "visible" },
-          { duration: 0.5, yPercent: -50, opacity: 1 }
-        )
-        .to(
-          ".page__mask",
-          { duration: 0.5, opacity: 0.5, visibility: "visible" },
-          "<"
-        );
-    }
-    isOverlayOpen = true;
-  }
-  /*
-  if (checkOnMobile()) {
-    // Why are there single-item arrays here?
-    // Because Javascript will spread them into chararrays otherwise.
-    overlay = new Slidedown(
-      ["slidedown", "theme_picker__slidedown"],
-      ["theme_picker__preview"]
-    );
-  } else {
-    overlay = new Modal(
-      ["modal", "theme_picker__modal"],
-      ["theme_picker__preview"]
-    );
-  }*/
+  let themePicker = document.querySelector(".theme_picker__modal");
+  themes.forEach((theme) => {
+    themePicker.appendChild(createThemePreview(theme));
+  });
+  document.body.appendChild(themePicker);
+  let openTimeline = openOverlay(modalAnimation);
 });
-/*
-  let currentTheme = localStorage.getItem("currentTheme");
-  if (currentTheme === "lights-out") {
-    resetTheme();
-    applyTheme("blinding-lights");
-  } else if (currentTheme === "blinding-lights") {
-    resetTheme();
-    applyTheme("lights-out");
-  }
-});
-*/
-
-function lol() {
-  overlay.switchType(
-    ["slidedown", "theme_picker__slidedown"],
-    ["theme_picker__preview"]
-  );
-}
